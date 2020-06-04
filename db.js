a503c9b9332be4a750706840d4adda2e470a82c6
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-04 09:19:34 +0100

    initial commit

---
+++ db.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const mongodb = require('mongodb')
+
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client
+  const app = require('./app')
+  console.log(`listening on port ${process.env.PORT}`)
+  app.listen(process.env.PORT)
+})
